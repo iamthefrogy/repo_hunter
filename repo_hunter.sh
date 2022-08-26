3c76eb96372cdbe9e7049857602507249440e8bb
Chintan Gurjar <chintangurjar@outlook.com>
2022-08-26 05:24:05 +0100

    Update repo_hunter.sh

--- repo_hunter.sh
+++ repo_hunter.sh
@@ -5,9 +5,14 @@ read input
 input1=`echo $input | tr '[:upper:]' '[:lower:]'| tr " " "+"`
 topic=`echo $input1 | tr " " "+"`
 repos=$(curl -s "https://api.github.com/search/repositories?q=stars%3A%3E50+$topic+pushed%3A%3E2022-01-01+sort:stars+in%3Aname" | jq -r '.items[].full_name')
+if [ -e repo_list.txt ] ; then
+        rm repo_list.txt
+else
+        :
+fi
 for repo in $repos
 do
-echo "Checking $repo"
+echo "Checking $repo" | tee -a repo_list.txt
 folder=`echo $repo | cut -d "/" -f2`
 if [ -d "$folder" ]; then
   echo -e "\e[36mRepo exists, updating...\e[0m"
@@ -19,3 +24,12 @@ else
   git clone "https://github.com/$repo" -q
 fi
 done
+
+
+if [ -e repo_list.txt ] ; then
+        echo -e "\e[93mTotal repos found: \e[32m$(cat repo_list.txt | wc -l)\e[0m"
+        echo -e "\e[93mList of Repos:\e[0m"
+        cat repo_list.txt | cut -d " " -f2
+else
+        :
+fi
