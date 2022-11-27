68b928c3a621269fbee65f537221c17132428a2a
Chintan Gurjar <chintangurjar@outlook.com>
2022-11-27 00:53:14 +0000

    Update repo_hunter.sh

--- repo_hunter.sh
+++ repo_hunter.sh
@@ -1,35 +1,28 @@
 #!/bin/bash
 
-echo "What topic you are interested in?"
+echo -e "\e[93mWhat topic you are interested in?\e[0m"
 read input
 input1=`echo $input | tr '[:upper:]' '[:lower:]'| tr " " "+"`
 topic=`echo $input1 | tr " " "+"`
-repos=$(curl -s "https://api.github.com/search/repositories?q=stars%3A%3E50+$topic+pushed%3A%3E2022-01-01+sort:stars+in%3Aname" | jq -r '.items[].full_name')
-if [ -e repo_list.txt ] ; then
-        rm repo_list.txt
+
+tpc=$(curl -s "https://api.github.com/search/repositories?q=stars%3A%3E50+$topic+sort:stars+in%3Aname&per_page=5" | jq -r '.total_count')
+
+if [ -z "$tpc" ]
+then
+        echo "Sorry, no results found"
+        exit 1
 else
-        :
+        pg=$(($tpc/5))
 fi
-for repo in $repos
+
+for i in $(seq 1 $pg);
 do
-echo "Checking $repo" | tee -a repo_list.txt
-folder=`echo $repo | cut -d "/" -f2`
-if [ -d "$folder" ]; then
-  echo -e "\e[36mRepo exists, updating...\e[0m"
-  cd $folder
-  git pull -q
-  cd ..
-else
-  echo -e "\e[92mRepo does not exist, clonning...\e[0m"
-  git clone "https://github.com/$repo" -q
-fi
+repos=$(curl -s "https://api.github.com/search/repositories?q=stars%3A%3E50+$topic+sort:stars+in%3Aname&per_page=5&page=$i" | jq -r '.items[].html_url')
+echo "$repos" | tee -a $topic-repos.txt
 done
 
-
-if [ -e repo_list.txt ] ; then
-        echo -e "\e[93mTotal repos found: \e[32m$(cat repo_list.txt | wc -l)\e[0m"
-        echo -e "\e[93mList of Repos:\e[0m"
-        cat repo_list.txt | cut -d " " -f2
+if [ -e $topic-repos.txt ] ; then
+        echo -e "\e[93mTotal repos found: \e[32m$(cat $topic-repos.txt | wc -l)\e[0m"
 else
         :
 fi
